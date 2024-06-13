59981fec4ca9f2539d3975854b9c999e40f3f362
jeonha01 <kangjeonha@gmail.com>
2024-06-14 01:26:18 +0900

    second commit

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ const first1 = document.getElementById('first1')
 
 function pictureClick(event) {
     const clickedImageSrc = event.target.getAttribute('src');
-    const Imagestring = plublem[randomnum].Answer.substring(0, 2)
+    const Imagestring = plublem[randomnum].Answer.replace('.png', '')
 
     if (clickedImageSrc === plublem[randomnum].Answer) {
         result1.textContent = '정답입니다.'
@@ -45,4 +45,54 @@ function showPicture() {
 }
 
 showPicture()
+
+///////////////////
+
+const plublemm = [
+    {
+        Question: "2. 나래는 살을 빼기 위해 수영장을 다니기로 했습니다. 나래가 챙겨야 할 준비물로 알맞지 않은 것은 무엇인가요?",
+        Choice: ["수영모.png", "등산스틱.png", "물안경.png", "수영복.png"],
+        Answer: "등산스틱.png",
+        number: 2
+    }, {
+        Question: "2. 민주는 여름에 입을 옷을 고르려고 합니다. 다음 중 여름에 어울리지 않는 옷은 무엇인가요?",
+        Choice: ["운동복.png", "패딩.png", "반팔.png", "반바지.png"],
+        Answer: "패딩.png",
+        number: 2
+    }, {
+        Question: "2. 민재는 일본으로 여행을 갔습니다. 다음 사진 중 일본에서 볼 수 있는 풍경으로 알맞지 않은 것은 무엇인가요?",
+        Choice: ["사막.png", "초밥.png", "후지산.png", "일본건물.png"],
+        Answer: "사막.png",
+        number: 1
+    }
+]
+
+const result2 = document.getElementById('result2')
+let randomnumm = Math.floor(Math.random() * 3)
+const second2 = document.getElementById('second2')
+
+function pictureClickk(event) {
+    const clickedImageSrc = event.target.getAttribute('src');
+    const Imagestring = plublemm[randomnumm].Answer.replace('.png', '')
+
+    if (clickedImageSrc === plublemm[randomnumm].Answer) {
+        result2.textContent = '정답입니다.'
+    } else {
+        result2.textContent = `틀렸습니다. 정답은 ${plublemm[randomnumm].number}번 ${Imagestring}`
+    }
+}
+
+function showPicturee() {
+    second2.textContent = plublemm[randomnumm].Question
+
+    const currentQuestion = plublemm[randomnumm];
+    const Choices = currentQuestion.Choice;
+
+    for (let i = 0; i < Choices.length; i++) {
+        const image = document.getElementById(`pic${i + 1}`)
+        image.setAttribute('src', plublemm[randomnumm].Choice[i])
+    }
+}
+
+showPicturee()
 
